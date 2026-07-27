6f0260d5039a0e1c503b977c7c123f4bda366882
Nithish2754 <sec24sc019@sairamtap.edu.in>
2026-07-27 21:59:56 +0530

    Install spaCy model via requirements.txt to fix permission error

--- App/App.py
+++ App/App.py
@@ -19,10 +19,6 @@ from geopy.geocoders import Nominatim
 # libraries used to parse the pdf files
 import nltk
 nltk.download('stopwords')
-import spacy
-import spacy.cli
-if not spacy.util.is_package("en_core_web_sm"):
-    spacy.cli.download("en_core_web_sm")
 from pyresparser import ResumeParser
 try:
     from pdfminer.layout import LAParams, LTTextBox
